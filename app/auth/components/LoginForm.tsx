b04bf5ba725a85d76b033316699ba852961e0076
Valerius Mattfeld <mail@valerius.me>
2021-12-18 15:28:23 +0000

    update prisma schema

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,8 +1,8 @@
-import {AuthenticationError, Link, useMutation, Routes, PromiseReturnType} from "blitz"
-import {LabeledTextField} from "app/core/components/LabeledTextField"
-import {Form, FORM_ERROR} from "app/core/components/Form"
+import { AuthenticationError, Link, useMutation, Routes, PromiseReturnType } from "blitz"
+import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
-import {Login} from "app/auth/validations"
+import { Login } from "app/auth/validations"
 
 type LoginFormProps = {
   onSuccess?: (user: PromiseReturnType<typeof login>) => void
@@ -18,14 +18,14 @@ export const LoginForm = (props: LoginFormProps) => {
       <Form
         submitText="Login"
         schema={Login}
-        initialValues={{email: "", password: ""}}
+        initialValues={{ email: "", password: "" }}
         onSubmit={async (values) => {
           try {
             const user = await loginMutation(values)
             props.onSuccess?.(user)
           } catch (error: any) {
             if (error instanceof AuthenticationError) {
-              return {[FORM_ERROR]: "Sorry, those credentials are invalid"}
+              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
             } else {
               return {
                 [FORM_ERROR]:
@@ -38,14 +38,14 @@ export const LoginForm = (props: LoginFormProps) => {
         <LabeledTextField name="email" label="Email" placeholder="Email" />
         <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
         <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
+          {/* <Link href={Routes.ForgotPasswordPage()}> */}
+          <a>Forgot your password?</a>
+          {/* </Link> */}
         </div>
       </Form>
 
-      <div style={{marginTop: "1rem"}}>
-        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
+      <div style={{ marginTop: "1rem" }}>
+        {/* Or <Link href={Routes.SignupPage()}>Sign Up</Link> */}
       </div>
     </div>
   )
