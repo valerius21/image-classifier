c5763f67f7ec6ff748b0078925b29e59f161bd9d
Valerius Mattfeld <mail@valerius.me>
2021-12-18 17:54:06 +0100

    remove unused tables

---
+++ db/migrations/20211218164629_remove_unused_models/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Private` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Public` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Submission` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Private";
+
+-- DropTable
+DROP TABLE "Public";
+
+-- DropTable
+DROP TABLE "Submission";
